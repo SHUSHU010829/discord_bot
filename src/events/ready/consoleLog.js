e017e0474d30fb77ff2dbd56d195491cda2eeed3
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-02-20 00:40:50 +0800

    feat: 新增機器人狀態

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,5 +1,12 @@
 require("colors");
 
+const { ActivityType } = require("discord.js");
+
 module.exports = (client) => {
+  client.user.setActivity({
+    name: "直播 🍿",
+    type: ActivityType.Streaming,
+    url: "https://www.twitch.tv/shushu010829",
+  });
   console.log(`[INFO] ${client.user.username} is online!`.blue);
 };
