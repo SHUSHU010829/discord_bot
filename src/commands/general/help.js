5ef9f52dd461afa8583345aab1c44793af71aca5
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-02-20 00:35:02 +0800

    feat: 新增機器人help指令

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -1,23 +1,74 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+require("colors");
+
+const { readdirSync } = require("fs");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+const {footerText} = require("../../messageConfig.json");
+const buttonPaginator = require("../../utils/buttonPagination");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("逼逼機器人份內工作！")
-    .setDMPermission(false)
-    .toJSON(),
-
-  userPermissions: [PermissionFlagsBits.ManageMessages],
-  botPermissions: [PermissionFlagsBits.Connect],
+    .setDescription("逼逼機器人份內工作！"),
 
   run: async (client, interaction) => {
-    const msg = await interaction.reply({
-      content: "Pong! 🏓",
-      fetchReply: true,
-    });
-    const ping = msg.createdTimestamp - interaction.createdTimestamp;
-    interaction.editReply(
-      `Pong! 🏓\n機器人延遲：${ping} ms\n API 延遲：${client.ws.ping} ms`
-    );
+    try {
+        const commandFolders = readdirSync('./src/commands');
+        const helpEmbeds = [];
+
+        for (const folder of commandFolders) {
+            const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+
+            const categoryEmbed = new EmbedBuilder()
+            .setTitle(folder)
+            .setFooter({
+                text: `${footerText}`,
+            })
+            .setTimestamp()
+            .setThumbnail(client.user.displayAvatarURL())
+
+            const subcommands = [];
+
+            for (const file of commandFiles) {
+                const command = require(`./../${folder}/${file}`);
+                
+                if(command.deleted) {
+                    continue;
+                }
+
+                const description = `${command.data.description || "No description provided."}`;
+
+                if(command.data.type === "SUB_COMMAND" || command.data.type === "SUB_COMMAND_GROUP") {
+                    subcommands.push(command);           
+                } else {
+                    categoryEmbed.addFields([
+                      {
+                        name: `/${command.data.name}`,
+                        value: description,
+                      },
+                    ]);
+                }
+            }
+
+            if(subcommands.length > 0) {
+                categoryEmbed.addFields([
+                  {
+                    name: "Subcommands",
+                    value: subcommands
+                      .map((subcommand) => `/${subcommand.data.name}`)
+                      .join("\n"),
+                  },
+                ]);
+            }
+
+            helpEmbeds.push(categoryEmbed);
+        }
+
+        await buttonPaginator(interaction, helpEmbeds);
+    } catch (error) {
+      console.log(
+        `[ERROR] An error occurred inside the command ask:\n${error}`.red
+      );
+    }
   },
 };
