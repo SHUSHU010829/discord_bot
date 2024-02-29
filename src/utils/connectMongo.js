0cf589c09f33fe1c42d8b67a01efdba01d882654
FOR-Shu <annie550635@gmail.com>
2024-03-01 00:58:04 +0800

    feat: 情勒文生成指令

--- src/utils/connectMongo.js
+++ src/utils/connectMongo.js
@@ -8,12 +8,13 @@ module.exports = async (client) =>{
     try {
         await mongoClient.connect();
         const dbName = "MorningBot";
-        const collectionName = "FoodList";
         const database = mongoClient.db(dbName);
-        const collection = database.collection(collectionName);
+        const collection = database.collection("FoodList");
+        const gaslightCollection = database.collection("GaslightPost");
 
         client.database = database;
         client.collection = collection;
+        client.gaslightCollection = gaslightCollection;
         console.log(`[DATA] Successfully connected to MongoDB!`.cyan);
     } catch (error) {
         console.log(
