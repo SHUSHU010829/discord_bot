821ccb2392c148bd2f1c62bd025d2edc401ce7fa
SHUSHU010829 <shushu90829@gmail.com>
2024-04-24 12:58:34 +0800

    delete: 刪除投票指令

--- src/events/ready/connectDb.js
+++ src/events/ready/connectDb.js
@@ -1,7 +1,24 @@
 require("colors");
 
-const connectToMongoDB = require("../../utils/connectMongo");
+const { MongoClient } = require("mongodb");
 
-module.exports = (client) => {
-  connectToMongoDB(client);
+module.exports = async (client) => {
+  const uri = `mongodb+srv://SHUSHU:${process.env.MONGO_PASSWORD}@morningbot.ar55cbn.mongodb.net/?retryWrites=true&w=majority`;
+  const mongoClient = new MongoClient(uri);
+  try {
+    await mongoClient.connect();
+    const dbName = "MorningBot";
+    const database = mongoClient.db(dbName);
+    const collection = database.collection("FoodList");
+    const gaslightCollection = database.collection("GaslightPost");
+
+    client.database = database;
+    client.collection = collection;
+    client.gaslightCollection = gaslightCollection;
+    console.log(`[DATA] Successfully connected to MongoDB!`.cyan);
+  } catch (error) {
+    console.log(
+      `[ERROR] An error occurred inside the command ask:\n${error}`.red
+    );
+  }
 };
--- src/events/ready/sendMorningMessage.js
+++ src/events/ready/sendMorningMessage.js
@@ -1,7 +1,10 @@
 require("colors");
 
 const cron = require("node-cron");
-const createMorningMessage = require("../../utils/createMorningMessage");
+const { DateTime } = require("luxon");
+
+const calenderData = require("../../data/calender.json");
+const { normalChannelId } = require("../../config.json");
 
 module.exports = (client) => {
   // Schedule createMorningMessage to run every day at 10:00 AM
@@ -13,7 +16,40 @@ module.exports = (client) => {
   cron.schedule(
     "0 10 * * *",
     () => {
-      createMorningMessage(client);
+      const channel = client.channels.cache.get(normalChannelId);
+
+      if (channel) {
+        const formattedDate = DateTime.now()
+          .setZone("Asia/Taipei")
+          .toFormat("yyyy-MM-dd");
+        const fortuneList = ["大吉", "中吉", "小吉", "平凡無奇", "凶", "大凶"];
+        const randomFortune =
+          fortuneList[Math.floor(Math.random() * fortuneList.length)];
+
+        if (calenderData) {
+          const matchingData = calenderData.find(
+            (data) => data.date === formattedDate
+          );
+
+          if (matchingData && matchingData.is_holiday === true) {
+            if (matchingData.description === null) {
+              const message = `早安 <:FlushedHug:1220244873064742972> \n現在是 ${formattedDate} 10:00 A.M.\n逼逼機器人開工了！\n但今天是週末，大家可以繼續睡！！<a:nesuDance:1182636277602992169>\n今日抽卡運勢：**${randomFortune}** <:PrideFloat:1220032890658619452>  `;
+              channel.send(message);
+            } else {
+              const message = `早安 <:FlushedHug:1220244873064742972> \n現在是 ${formattedDate} 10:00 A.M.\n逼逼機器人開工了！\n但今天是${matchingData.description}，大家可以繼續睡！！<a:nesuDance:1182636277602992169>\n今日抽卡運勢：**${randomFortune}** <:PrideFloat:1220032890658619452>  `;
+              channel.send(message);
+            }
+          } else {
+            const message = `早安 <:FlushedHug:1220244873064742972> \n現在是 ${formattedDate} 10:00 A.M.\n逼逼機器人開工了！\n各位起床起床起床床！！<a:nesuDance:1182636277602992169>\n今日抽卡運勢：**${randomFortune}** <:PrideFloat:1220032890658619452>  `;
+            channel.send(message);
+          }
+        } else {
+          const message = `早安 <:FlushedHug:1220244873064742972> \n現在是 ${formattedDate} 10:00 A.M.\n逼逼機器人開工了！\n各位起床起床起床床！！<a:nesuDance:1182636277602992169>\n今日抽卡運勢：**${randomFortune}** <:PrideFloat:1220032890658619452> (今天太神秘了，找不到假期資料？)`;
+          channel.send(message);
+        }
+      } else {
+        console.log(`[ERROR] 早安訊息：無法找到目標頻道:\n${error}`.red);
+      }
     },
     {
       scheduled: true,
