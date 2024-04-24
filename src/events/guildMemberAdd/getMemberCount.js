372407b5db405dc343d0922a79912a2642236bd9
SHUSHU010829 <shushu90829@gmail.com>
2024-04-24 12:51:56 +0800

    feat: 新增刪除人數變動

---
+++ src/events/guildMemberAdd/getMemberCount.js
@@ -0,0 +1,18 @@
+require("colors");
+
+const { ActivityType } = require("discord.js");
+const { serverId } = require("../../config.json");
+
+module.exports = (client) => {
+  let guild = client.guilds.cache.get(serverId);
+  let memberCount = guild.memberCount;
+  let memberCountChannel = guild.channels.cache.get("1232546673448849500");
+  memberCountChannel
+    .setName(`😧 舒禮人數： ${memberCount}`)
+    .then((result) =>
+      console.log(`[SETTING] server user count set successfully!`.green)
+    )
+    .catch((error) =>
+      console.log(`[ERROR] user number count cannot set: ${error}`.red)
+    );
+};
--- src/events/ready/serverSetting.js
+++ src/events/ready/serverSetting.js
@@ -10,17 +10,5 @@ module.exports = (client) => {
     url: "https://www.twitch.tv/shushu010829",
   });
 
-  let guild = client.guilds.cache.get(serverId);
-  let memberCount = guild.memberCount;
-  let memberCountChannel = guild.channels.cache.get("1232546673448849500");
-  memberCountChannel
-    .setName(`😧 舒禮人數： ${memberCount}`)
-    .then((result) =>
-      console.log(`[SETTING] server user count set successfully!`.green)
-    )
-    .catch((error) =>
-      console.log(`[ERROR] user number count cannot set: ${error}`.red)
-    );
-
   console.log(`[INFO] ${client.user.username} is online!`.blue);
 };
