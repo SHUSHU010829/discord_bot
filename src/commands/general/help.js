dae05221cf3d9cc9aa9514b5bdb144827dab66c6
SHUSHU010829 <shushu90829@gmail.com>
2024-09-20 11:45:46 +0800

    fix: help command

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -3,68 +3,20 @@ require("colors");
 const { readdirSync } = require("fs");
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
-const {footerText} = require("../../messageConfig.json");
+const { footerText } = require("../../messageConfig.json");
 const buttonPaginator = require("../../utils/buttonPagination");
+const command = require("nodemon/lib/config/command");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("逼逼機器人份內工作！"),
+    .setDescription("逼逼機器人工作介紹！"),
 
   run: async (client, interaction) => {
     try {
-        const commandFolders = readdirSync('./src/commands');
-        const helpEmbeds = [];
-
-        for (const folder of commandFolders) {
-            const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
-
-            const categoryEmbed = new EmbedBuilder()
-            .setTitle(folder)
-            .setFooter({
-                text: `${footerText}`,
-            })
-            .setTimestamp()
-            .setThumbnail(client.user.displayAvatarURL())
-
-            const subcommands = [];
-
-            for (const file of commandFiles) {
-                const command = require(`./../${folder}/${file}`);
-                
-                if(command.deleted) {
-                    continue;
-                }
-
-                const description = `${command.data.description || "No description provided."}`;
-
-                if(command.data.type === "SUB_COMMAND" || command.data.type === "SUB_COMMAND_GROUP") {
-                    subcommands.push(command);           
-                } else {
-                    categoryEmbed.addFields([
-                      {
-                        name: `/${command.data.name}`,
-                        value: description,
-                      },
-                    ]);
-                }
-            }
-
-            if(subcommands.length > 0) {
-                categoryEmbed.addFields([
-                  {
-                    name: "Subcommands",
-                    value: subcommands
-                      .map((subcommand) => `/${subcommand.data.name}`)
-                      .join("\n"),
-                  },
-                ]);
-            }
-
-            helpEmbeds.push(categoryEmbed);
-        }
-
-        await buttonPaginator(interaction, helpEmbeds);
+      return interaction.reply({
+        command: "還在製作中...",
+      });
     } catch (error) {
       console.log(
         `[ERROR] An error occurred inside the command ask:\n${error}`.red
