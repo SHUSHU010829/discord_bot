954faea8439b8f135ca85fa71cc896b7be793a0b
SHUSHU010829 <shushu90829@gmail.com>
2024-10-15 17:46:53 +0800

    fix: 測試更改寫法回覆是否成功

--- src/commands/draw/lotto.js
+++ src/commands/draw/lotto.js
@@ -7,29 +7,32 @@ module.exports = {
     .setName("鹹魚翻身")
     .setDescription("不保證中獎樂透號碼... 🎰"),
 
-  run: async (client, interaction) => {
-    await interaction.deferReply();
+  run: async (interaction) => {
+    await interaction.reply({
+      content: "預測中... 🎰",
+      fetchReply: true,
+    });
+
+    const getLottoNumbers = () => {
+      const numbers = new Set();
+      // 隨機選擇六個不重複的號碼
+      while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 49) + 1);
+      }
+      // 從小到大排序
+      const sortedNumbers = [...numbers].sort((a, b) => a - b);
+      return sortedNumbers;
+    };
+
+    const lottoNumbers = getLottoNumbers();
 
     try {
-      const getLottoNumbers = () => {
-        const numbers = new Set();
-        // 隨機選擇六個不重複的號碼
-        while (numbers.size < 6) {
-          numbers.add(Math.floor(Math.random() * 49) + 1);
-        }
-        // 從小到大排序
-        const sortedNumbers = [...numbers].sort((a, b) => a - b);
-        return sortedNumbers;
-      };
-
-      const lottoNumbers = getLottoNumbers();
-
-      await interaction.reply(
+      interaction.editReply(
         `本期樂透 ➡️ \n\n${lottoNumbers.join(", ")}` +
           `\n\n祝您中大獎！🔥\n中了記得分舒舒，不客氣 ✨`
       );
     } catch (error) {
-      await interaction.reply("哎呀！今天不適合簽大樂透 💤");
+      interaction.editReply("哎呀！今天不適合簽大樂透 💤");
       console.log(
         `[ERROR] An error occurred inside the lotto ask:\n${error}`.red
       );
